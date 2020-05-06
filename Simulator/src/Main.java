342bb4df99e177736799bfa1195cc8a3b82356a3
Alex <leehyunjun122@gmail.com>
2020-05-05 23:04:26 -0700

    added some more things like button

--- Simulator/src/Main.java
+++ Simulator/src/Main.java
@@ -2,6 +2,8 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Main {
 	public static void main(String[] args) {
@@ -19,8 +21,19 @@ public class Main {
 	private static void placeComponents(JPanel panel) {
 		panel.setLayout(null);
 		JLabel title = new JLabel("Welcome to the simulator");
-		title.setBounds(100,30,1000,25);
+		title.setBounds(100,30,160,25);
         panel.add(title);
         
+        JLabel enter = new JLabel("Type here:");
+		enter.setBounds(600,80,160,25);
+        panel.add(enter);
+        
+        JTextField userText = new JTextField(20);
+        userText.setBounds(600,105,300,110);
+        panel.add(userText);
+        
+        JButton button = new JButton("Enter");
+        button.setBounds(700, 220, 80, 25);
+        panel.add(button);
 	}
 }
