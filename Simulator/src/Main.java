2ad4637c7ded15cc1d45f4823565b6af330478bc
Alex <leehyunjun122@gmail.com>
2020-05-09 16:52:04 -0700

    working on how something can be stored to be used

--- Simulator/src/Main.java
+++ Simulator/src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.Dimension;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -6,6 +8,9 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 
 public class Main {
+	private static JTextField userText;
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
         JFrame frame = new JFrame("simulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,12 +33,30 @@ public class Main {
 		enter.setBounds(600,80,160,25);
         panel.add(enter);
         
-        JTextField userText = new JTextField(20);
+        userText = new JTextField(20);
         userText.setBounds(600,105,300,110);
         panel.add(userText);
         
         JButton button = new JButton("Enter");
         button.setBounds(700, 220, 80, 25);
         panel.add(button);
+        
+        JLabel writing = new JLabel();
+		writing.setBounds(100,40,160,25);
+        for(int i=0;i<5;i++) {
+        	String expression = input.nextLine();
+        	for (long stop=System.nanoTime()+TimeUnit.SECONDS.toNanos(2);stop>System.nanoTime();) {
+        			writing = new JLabel(expression);
+        			panel.add(writing);
+        	}
+        }
+	}
+	
+	public static boolean writingChecker(JTextField statement) {
+		if(statement != null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
